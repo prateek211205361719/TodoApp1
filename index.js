b05fc7dc609325f019810f7096893642ab2137ec
ceptes <you@example.comprateek@ceptes.com>
2018-02-05 19:31:36 +0530

    test commit

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.get('/', (req, res) => {
 //get all list
 app.get('/todos', async (req, res) => {
       oauth2 = new jsforce.OAuth2({
-            loginUrl:'https://login.salesforce.com', 
+            loginUrl:'https://sumit8493-dev-ed.my.salesforce.com', 
             clientId : keys.clientId,
             clientSecret : keys.clientSecret,
             redirectUri : 'https://todo211205.herokuapp.com/oauth2/callback'
